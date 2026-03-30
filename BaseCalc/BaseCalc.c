656a0246a5ad0b5a087eb780205ee931f545b65b
TheRealBYK <byknintendo@gmail.com>
2026-03-30 15:57:32 +0300

    A base calculator made in GTK4 and C

---
+++ BaseCalc/BaseCalc.c
@@ -0,0 +1,235 @@
+#include "BaseCalc.h"
+
+// void pop_menu(GtkButton *button, gpointer data)
+// {
+//     gtk_popover_popup(GTK_POPOVER (data));
+// }
+
+typedef struct {
+    GtkWidget *window;
+    GtkWidget *convertFrom;
+    GtkWidget *convertTo;
+    GtkWidget *input;
+    GtkWidget *output;
+} PassWidgets;
+
+void set_base(GtkButton *button, gpointer data)
+{
+    const char *str = gtk_button_get_label(GTK_BUTTON (button));
+    gtk_menu_button_set_label(GTK_MENU_BUTTON (data), str);
+}
+
+void convert_dec_2_base_X(GtkButton *button, gpointer data)
+{
+    PassWidgets *widgetStruct = (PassWidgets *)data;
+    const char *from = gtk_menu_button_get_label(GTK_MENU_BUTTON (widgetStruct->convertFrom));
+    const char *to = gtk_menu_button_get_label(GTK_MENU_BUTTON (widgetStruct->convertTo));
+    const char *str2Convert = gtk_entry_buffer_get_text(gtk_entry_get_buffer(GTK_ENTRY (widgetStruct->input)));
+    GtkAlertDialog *alert;
+    bool dec2bX = false;
+    int base = 0;
+    char baseStr[32] = {0};
+    if (strcmp(to, from) == 0)
+    {
+	alert = gtk_alert_dialog_new("Converting from %s base to %s base, pointless.\n", to, from);
+	gtk_alert_dialog_show(GTK_ALERT_DIALOG (alert), GTK_WINDOW (widgetStruct->window));
+	return;
+	// gtk_label_set_label(GTK_LABEL (widgetStruct->output), "Cannot convert from base t");
+    }
+    else
+    {
+	if (strcmp(from, "Binary") == 0)
+	{
+	    base = 2;
+	    if (strcmp(to, "Decimal") == 0)
+	    {
+		snprintf(baseStr, sizeof(baseStr), "%d", baseX2Dec(2, str2Convert, strlen(str2Convert)));
+		printf("%s\n", str2Convert);
+		gtk_button_set_label(GTK_BUTTON (widgetStruct->output), baseStr);
+	    }
+	    else
+	    {
+		alert = gtk_alert_dialog_new("Currently %s Conversion is limited to base 10 only.\n", from);
+		gtk_alert_dialog_show(GTK_ALERT_DIALOG (alert), GTK_WINDOW (widgetStruct->window));
+		return;
+	    }
+	}
+	else if (strcmp(from, "Octal") == 0)
+	{
+	    base = 8;
+	    if (strcmp(to, "Decimal") == 0)
+	    {
+		snprintf(baseStr, sizeof(baseStr), "%d", baseX2Dec(base, str2Convert, strlen(str2Convert)));
+		gtk_button_set_label(GTK_BUTTON (widgetStruct->output), baseStr);
+	    }
+	    else
+	    {
+		alert = gtk_alert_dialog_new("Currently %s Conversion is limited to base 10 only.\n", from);
+		gtk_alert_dialog_show(GTK_ALERT_DIALOG (alert), GTK_WINDOW (widgetStruct->window));
+		return;
+	    }
+	}
+	else if (strcmp(from, "Decimal") == 0)
+	{
+	    dec2bX = true;
+	    int value = atoi(str2Convert);
+	    if (strcmp(to, "Binary") == 0)
+	    {
+		base = 2;
+	    }
+	    else if (strcmp(to, "Octal") == 0)
+	    {
+		base = 8;
+	    }
+	    else if (strcmp(to, "Hexadecimal") == 0)
+	    {
+		base = 16;
+	    }
+	    else
+	    {
+		alert = gtk_alert_dialog_new("Unrecognized Option: %s\n", to);
+		gtk_alert_dialog_show(GTK_ALERT_DIALOG (alert), GTK_WINDOW (widgetStruct->window));
+		return;
+	    }
+	    dec2BaseX(base, value, GTK_BUTTON (widgetStruct->output));
+	}
+	else if (strcmp(from, "Hexadecimal") == 0)
+	{
+	    base = 16;
+	    if (strcmp(to, "Decimal") == 0)
+	    {
+		snprintf(baseStr, sizeof(baseStr), "%d", baseX2Dec(base, str2Convert, strlen(str2Convert)));
+		gtk_button_set_label(GTK_BUTTON (widgetStruct->output), baseStr);
+	    }
+	    else
+	    {
+		alert = gtk_alert_dialog_new("Currently %s Conversion is limited to base 10 only.\n", from);
+		gtk_alert_dialog_show(GTK_ALERT_DIALOG (alert), GTK_WINDOW (widgetStruct->window));
+		return;
+	    }
+	}
+	else
+	{
+	    alert = gtk_alert_dialog_new("Unrecognized Option: %s\n", from);
+	    gtk_alert_dialog_show(GTK_ALERT_DIALOG (alert), GTK_WINDOW (widgetStruct->window));
+	    return;
+	}
+    }
+}
+
+void copy_output(GtkButton *button, gpointer data)
+{
+    const char *text = gtk_button_get_label(GTK_BUTTON (button));
+    GdkClipboard *clipboard = gtk_widget_get_clipboard(GTK_WIDGET (button));
+    gdk_clipboard_set_text(GDK_CLIPBOARD (clipboard), text);
+}
+
+PassWidgets widgets;
+
+void activate (GtkApplication *app, gpointer data)
+{
+    GtkWidget *window = gtk_application_window_new(app);
+    GtkWidget *grid = gtk_grid_new();
+
+    GtkWidget *entryIn = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY (entryIn), "Input");
+    GtkWidget *labelOut = gtk_button_new_with_label("Output");
+    GtkWidget *convertBtn = gtk_button_new_with_label("Convert");
+    
+    GtkWidget *popUpSelection1 = gtk_menu_button_new();
+    GtkWidget *popOver1 = gtk_popover_new();
+    GtkWidget *box1 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 1);
+
+    GtkWidget *bBtn1 = gtk_button_new_with_label("Binary");
+    GtkWidget *oBtn1 = gtk_button_new_with_label("Octal");
+    GtkWidget *dBtn1 = gtk_button_new_with_label("Decimal");
+    GtkWidget *xBtn1 = gtk_button_new_with_label("Hexadecimal");
+
+    GtkWidget *popUpSelection2 = gtk_menu_button_new();
+    GtkWidget *popOver2 = gtk_popover_new();
+    GtkWidget *box2 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 1);
+
+    GtkWidget *bBtn2 = gtk_button_new_with_label("Binary");
+    GtkWidget *oBtn2 = gtk_button_new_with_label("Octal");
+    GtkWidget *dBtn2 = gtk_button_new_with_label("Decimal");
+    GtkWidget *xBtn2 = gtk_button_new_with_label("Hexadecimal");
+
+    widgets.window = GTK_WIDGET (window);
+    widgets.convertFrom = GTK_WIDGET (popUpSelection1);
+    widgets.convertTo = GTK_WIDGET (popUpSelection2);
+    widgets.input = GTK_WIDGET (entryIn);
+    widgets.output = GTK_WIDGET (labelOut);
+
+    gtk_window_set_title(GTK_WINDOW (window), "Base Calculator");
+    gtk_window_set_default_size(GTK_WINDOW (window), 200, 250);
+
+    gtk_entry_set_max_length(GTK_ENTRY (entryIn), 0);
+
+    gtk_window_set_child(GTK_WINDOW (window), grid);
+
+    gtk_popover_set_position(GTK_POPOVER (popOver1), GTK_POS_BOTTOM);
+    gtk_menu_button_set_popover(GTK_MENU_BUTTON (popUpSelection1), GTK_WIDGET (popOver1));
+    gtk_popover_set_child(GTK_POPOVER (popOver1), GTK_WIDGET (box1));
+
+    gtk_box_append(GTK_BOX (box1), GTK_WIDGET (bBtn1));
+    gtk_box_append(GTK_BOX (box1), GTK_WIDGET (oBtn1));
+    gtk_box_append(GTK_BOX (box1), GTK_WIDGET (dBtn1));
+    gtk_box_append(GTK_BOX (box1), GTK_WIDGET (xBtn1));
+
+    gtk_popover_set_position(GTK_POPOVER (popOver2), GTK_POS_BOTTOM);
+    gtk_menu_button_set_popover(GTK_MENU_BUTTON (popUpSelection2), GTK_WIDGET (popOver2));
+    gtk_popover_set_child(GTK_POPOVER (popOver2), GTK_WIDGET (box2));
+
+    gtk_box_append(GTK_BOX (box2), GTK_WIDGET (bBtn2));
+    gtk_box_append(GTK_BOX (box2), GTK_WIDGET (oBtn2));
+    gtk_box_append(GTK_BOX (box2), GTK_WIDGET (dBtn2));
+    gtk_box_append(GTK_BOX (box2), GTK_WIDGET (xBtn2));
+
+    gtk_grid_set_row_homogeneous(GTK_GRID (grid), TRUE);
+    gtk_grid_set_column_homogeneous(GTK_GRID (grid), TRUE);
+    gtk_grid_set_row_spacing(GTK_GRID (grid), 50);
+
+    gtk_grid_attach(GTK_GRID (grid), entryIn, 1, 1, 2, 1);
+    gtk_grid_attach(GTK_GRID (grid), labelOut, 6, 1, 2, 1);
+    gtk_grid_attach(GTK_GRID (grid), popUpSelection1, 1, 4, 2, 1);
+    gtk_grid_attach(GTK_GRID (grid), popUpSelection2, 6, 4, 2, 1);
+    gtk_grid_attach(GTK_GRID (grid), convertBtn, 3, 6, 1, 1);
+
+    // g_signal_connect(GTK_MENU_BUTTON (popUpSelection1), "activate", G_CALLBACK (pop_menu), popOver1);
+    // g_signal_connect(GTK_MENU_BUTTON (popUpSelection2), "activate", G_CALLBACK (pop_menu), popOver2);
+
+    g_signal_connect(GTK_BUTTON (labelOut), "clicked", G_CALLBACK (copy_output), NULL);
+
+    g_signal_connect(GTK_BUTTON (bBtn1), "clicked", G_CALLBACK (set_base), popUpSelection1);
+    g_signal_connect(GTK_BUTTON (oBtn1), "clicked", G_CALLBACK (set_base), popUpSelection1);
+    g_signal_connect(GTK_BUTTON (dBtn1), "clicked", G_CALLBACK (set_base), popUpSelection1);
+    g_signal_connect(GTK_BUTTON (xBtn1), "clicked", G_CALLBACK (set_base), popUpSelection1);
+
+    g_signal_connect(GTK_BUTTON (bBtn2), "clicked", G_CALLBACK (set_base), popUpSelection2);
+    g_signal_connect(GTK_BUTTON (oBtn2), "clicked", G_CALLBACK (set_base), popUpSelection2);
+    g_signal_connect(GTK_BUTTON (dBtn2), "clicked", G_CALLBACK (set_base), popUpSelection2);
+    g_signal_connect(GTK_BUTTON (xBtn2), "clicked", G_CALLBACK (set_base), popUpSelection2);
+
+    g_signal_connect(GTK_BUTTON (convertBtn), "clicked", G_CALLBACK (convert_dec_2_base_X), &widgets);
+
+    gtk_window_present(GTK_WINDOW (window));
+}
+
+
+int main(int argc, char **argv)
+{
+    GtkApplication *baseCalc = gtk_application_new("Base.Calculator", G_APPLICATION_DEFAULT_FLAGS);
+    int status;
+
+    g_signal_connect(G_APPLICATION(baseCalc), "activate", G_CALLBACK(activate), NULL);
+
+    status = g_application_run(G_APPLICATION(baseCalc), argc, argv);
+
+    g_object_unref(baseCalc);
+    widgets.convertFrom = NULL;
+    widgets.convertTo = NULL;
+    widgets.input = NULL;
+    widgets.output = NULL;
+
+    return status;
+}
---
+++ BaseCalc/BaseCalc.h
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+#include <gtk/gtk.h>
+
+int baseX2Dec (int base, const char *binStr, size_t strSize)
+{
+    // printf("%d\n", base);
+    char upBinStr[strSize];
+    int value = 0;
+    int power = strSize - 1;
+    for (int i = 0; i < strSize; i++)
+    {
+	int numValue = binStr[i] - '0';
+	upBinStr[i] = toupper(binStr[i]);
+	int hexValue = (upBinStr[i] - '0') - 7;
+	// printf("%d\n", hexValue);
+	// printf("%d\n", base);
+	if (numValue < 10)
+	{
+	    if (numValue >= base)
+	    {
+		printf("Illegal value.\n");
+		return -1;
+	    }
+	    else
+	    {
+		int multiplier = numValue;
+		value += pow(base, power) * multiplier;
+		// printf("%d\n", value);
+		power--;
+	    }
+	}
+	else
+	{
+	    if (hexValue >= base)
+	    {
+		printf("Illegal value.\n");
+		return -1;
+	    }
+	    else
+	    {
+		int multiplier = (upBinStr[i] - '0') - 7;
+		int tmpValue = pow(base, power) * multiplier;
+		value += tmpValue;
+		// printf("%d %d\n", tmpValue, value);
+		power--;
+	    }
+	}
+	// printf("%c\n", binStr[i]);
+    }
+    printf("%d\n", value);
+    return value;
+    // printf("%d\n%s\n", strSize, binStr);
+}
+
+void dec2BaseX(int base, int decimal, GtkButton *button)
+{
+    int IDX = 0;
+    char baseXStrRev[32] = {0};
+    char baseXStr[32] = {0};
+    int divide = decimal / base;
+    while (divide != 0)
+    {
+	if (decimal % base != 0)
+	{
+	    if (decimal % base < 10)
+	    {
+		snprintf(&baseXStrRev[IDX], sizeof(&baseXStrRev[IDX]), "%d", decimal % base);
+		IDX++;
+	    }
+	    else
+	    {
+		baseXStrRev[IDX] = '7' + (decimal % base);
+		IDX++;
+	    }
+	}
+	else
+	{
+	    baseXStrRev[IDX] = '0';
+	    IDX++;
+	}
+	decimal /= base;
+	divide = decimal / base;
+    }
+    
+    if (decimal % base != 0)
+    {
+	if (decimal % base < 10)
+	{
+	    snprintf(&baseXStrRev[IDX], sizeof(&baseXStrRev[IDX]), "%d", decimal % base);
+	    IDX++;
+	}
+	else
+	{
+	    baseXStrRev[IDX] = '7' + (decimal % base);
+	    IDX++;
+	}
+    }
+    else
+    {
+	baseXStrRev[IDX] = '0';
+	IDX++;
+    }
+    decimal /= base;
+    divide = decimal / base;
+
+    for (int i = (strlen(baseXStrRev) + 1); i >= 0; i--)
+    {
+	// printf("%c", baseXStrRev[i]);
+	baseXStr[(strlen(baseXStrRev) - 1) - i] = baseXStrRev[i];
+	// printf("%d\n", (strlen(baseXStrRev)) -i);
+	printf("i: %d\n", i);
+	printf("bin: %c\n", baseXStrRev[i]);
+
+    }
+
+    printf("DBG: %s\n", baseXStr);
+    printf("\n");
+
+    gtk_button_set_label(GTK_BUTTON (button), baseXStr);
+
+    // printf("%s\n", baseXStr);
+}
